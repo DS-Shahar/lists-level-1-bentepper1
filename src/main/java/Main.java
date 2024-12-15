0e6493817ea9633f7df8de74dc8a520754fe1969
bentepper1 <bentepper2008@gmail.com>
2024-12-15 11:53:41 +0200

    Add files via upload

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,57 @@
-class Main {
-  public static void main(String[] args) {
-    System.out.println("Hello World!");
-  }
+public class Main {	
+	
+	public static void main(String[] args) {
+		int [] a = {4,9,3,8};
+		Node<Integer> x = buildList(a);
+		ex2c(x);
+	}
+	
+	public static void ex2a(Node<Integer> p) {
+		while ( p != null) {
+			System.out.println(p.getValue());
+			p = p.getNext();
+		}
+	}
+	
+	public static void ex2b(Node<Integer> p) {
+		if (p != null) {
+			System.out.println(p.getValue());
+			ex2b(p.getNext());
+		}
+	}
+	
+	public static void ex2c(Node<Integer> p) {
+		if (p != null) {
+			ex2c(p.getNext());
+			System.out.println(p.getValue());
+		}
+	}
+	
+	public static Node<Integer> buildList(int[] a) {
+		Node<Integer> h = new Node<Integer>(-1);
+		Node<Integer> p = h;
+		Node<Integer> x;
+		for (int i = 0; i < a.length; i++) {
+			x = new Node<Integer>(a[i]);
+			p.setNext(x);
+			p = p.getNext();
+		}
+		return h.getNext();
+	}
+	
+	public static Node<Integer> delSingle(Node<Integer> h, int target) {
+		h = new Node<Integer>(-1, h);
+		Node<Integer> p = h;
+		boolean done = false;
+		while (!done && p.hasNext()) {
+			if (p.getNext().getValue() == target) {
+				p.setNext(p.getNext().getNext());
+				done = true;
+			} else {
+				p = p.getNext();
+			}
+		}
+		return h.getNext();
+	}
+	
 }
--- src/main/java/Node.java
+++ src/main/java/Node.java
@@ -1,38 +1,41 @@
-public class Node<T> {
-  private T value;
-  private Node<T> next;
 
-  public Node(T value) {
-    this.value = value;
-    this.next = null;
-  }
+	public class Node<T> {
+		  private T value;
+		  private Node<T> next;
 
-  public Node(T value, Node<T> next) {
-    this.value = value;
-    this.next = next;
-  }
+		  public Node(T value) {
+		    this.value = value;
+		    this.next = null;
+		  }
 
-  public T getValue() {
-    return this.value;
-  }
+		  public Node(T value, Node<T> next) {
+		    this.value = value;
+		    this.next = next;
+		  }
 
-  public Node<T> getNext() {
-    return this.next;
-  }
+		  public T getValue() {
+		    return this.value;
+		  }
 
-  public boolean hasNext() {
-    return (this.next != null);
-  }
+		  public Node<T> getNext() {
+		    return this.next;
+		  }
 
-  public void setValue(T value) {
-    this.value = value;
-  }
+		  public boolean hasNext() {
+		    return (this.next != null);
+		  }
 
-  public void setNext(Node<T> next) {
-    this.next = next;
-  }
+		  public void setValue(T value) {
+		    this.value = value;
+		  }
 
-  public String toString() {
-    return value + " ==> " + next;
-  }
-}
+		  public void setNext(Node<T> next) {
+		    this.next = next;
+		  }
+
+		  public String toString() {
+		    return value + " ==> " + next;
+		  }
+		}
+
+
